3c78e52ef5e58fe981775c7acdb1ce1ea610cfaa
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-11-14 16:00:14 +0000

    made: students registration

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ url = 'http://172.31.60.129:8001/api/v1/auth/login/'
 
 load_dotenv()
 
+base_url = 'http://172.31.60.129:8001/api/v1/'
+
 
 def main():
     print(f"username: {os.getenv('APP_USERNAME')}")
     response = requests.post(
-        url=url,
+        # url=url,
+        url=f'{base_url}auth/login/',
         headers={
             'accept': 'application/json',
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -31,17 +34,18 @@ def main():
         print("Response:")
         pprint(data)
 
-        TOKEN = data.get('access_token')
-        print(f'TOKEN: {TOKEN}')
+        token = data.get('access_token')
+        print(f'token: {token}')
     else:
         print(f'Error: {response.status_code}, {response.text}')
         exit(1)
 
     response = requests.get(
-        url='http://localhost:8001/api/v1/student/?page_number=0&page_size=10',
+        # url='http://localhost:8001/api/v1/student/?page_number=0&page_size=10',
+        url=f'{base_url}student/?page_number=0&page_size=10',
         headers={
             'accept': 'application/json',
-            'Authorization': f'Bearer {TOKEN}'
+            'Authorization': f'Bearer {token}'
         })
 
     if response.status_code == 200:
@@ -52,6 +56,43 @@ def main():
     else:
         print(f'Error: {response.status_code}, {response.text}')
         exit(1)
+
+    # register_url = 'http://localhost:8001/api/v1/user/register/'
+    register_url = f'{base_url}user/register/'
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    num_students = 10
+
+    for i in range(num_students):
+        # Форматирование данных для каждого студента с уникальными значениями на основе итератора i
+        student_data = {
+            "email": f"student2{i}@example.com",
+            "password": f"passwd2{i}",
+            "firstname": f"student2{i}",
+            "lastname": f"studentov2{i}",
+            "surname": f"studentovich2{i}",
+            "birthday": f"2020-10-{10 + i:02}T15:41:16.490Z",  # уникальная дата на основе i
+            "is_man": True,
+            "contact": "string"
+        }
+
+        # Отправка POST-запроса для регистрации студента
+        response = requests.post(
+            url=register_url,
+            headers=headers,
+            json=student_data
+        )
+
+        # Проверка ответа
+        if response.status_code == 200:
+            print(f"Student {i} registered successfully.")
+            pprint(response.json())
+        else:
+            print(f'Error registering student {i}: {response.status_code}, {response.text}')
 
 
 if __name__ == "__main__":
