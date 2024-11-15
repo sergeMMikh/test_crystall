ceb52bc78708681eaf3a036a583e0842cf3f91c5
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-11-15 19:55:01 +0000

    added: create_check_list

--- lessons.py
+++ lessons.py
@@ -49,3 +49,34 @@ def create_lesson(base_url: str,
         pprint(response.json())
     else:
         print(f'Error creating lesson: {response.status_code}, {response.text}')
+
+
+def create_check_list(base_url: str,
+                      token: str):
+    register_url = f'{base_url}lesson/'
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    lesson_data = {
+        "space_id": 1,
+        "trainer_id": 1,
+        "trainer_comments": "message",
+        "start": "2024-11-15T16:39:40.975Z"
+    }
+
+    # Отправка POST-запроса для регистрации студента
+    response = requests.post(
+        url=register_url,
+        headers=headers,
+        json=lesson_data
+    )
+
+    # Проверка ответа
+    if response.status_code == 200:
+        print(f"Lesson registered successfully.")
+        pprint(response.json())
+    else:
+        print(f'Error creating lesson: {response.status_code}, {response.text}')
--- main.py
+++ main.py
@@ -44,5 +44,7 @@ def main():
                     token=token)
 
 
+
+
 if __name__ == "__main__":
     main()
